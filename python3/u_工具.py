9835f4423f7f2b9e8256989a890889edfe1fa687
hlmio <52530012+hl-mio@users.noreply.github.com>
2020-10-30 22:06:00 +0800

    Update u_工具.py

--- python3/u_工具.py
+++ python3/u_工具.py
@@ -86,6 +86,17 @@ def getDictValue(my_dict, key="", default=None, 分隔符="."):
         return default
 
 
+# 设置多层dict的值
+def setDictValue(mydict,key,value,分隔符='.'):
+    keys = key.split(分隔符)
+    length = len(keys)
+    for index,i in enumerate(key.split(分隔符)):
+        if int(index)+1 == length:
+            mydict[i] = value
+        else:
+            mydict = mydict[i]
+
+
 # 获取当前时间的字符串
 def getCurrentDatetime_str(format_str="%Y-%m-%d %H:%M:%S"):
     return datetime.datetime.now().strftime(format_str)
@@ -135,7 +146,6 @@ def getDeepFilePaths(baseFilePath, ext="txt", is_deep = True, rst_filePaths=[]):
             .forEach(lambda dir: getDeepFilePaths(dir, ext, True, rst_filePaths))
 
     return rst_filePaths
-
 
 
 # endregion 未分类
@@ -404,12 +414,26 @@ class 配置类:
 
         return dict配置
 
-
     def _设置数据源(self,路径,类型,来源):
         self.数据源["路径"] = 路径
         self.数据源["类型"] = 类型.lower()
         self.数据源["来源"] = 来源.lower()
         return self
+
+    def _get_dict_keyLocation_list(self, mydict={}, 分隔符=".", 前缀="", rstList=[]):
+        for i in mydict.keys():
+            if not 前缀:
+                临时前缀 = f"{i}"
+            else:
+                临时前缀 = f"{前缀}{分隔符}{i}"
+
+            if isinstance(mydict[i], dict):
+                self._get_dict_keyLocation_list(mydict[i], 分隔符, 临时前缀, rstList)
+            else:
+                rstList.append(临时前缀)
+
+        return rstList
+
 
     def 加载(self,路径="./配置文件.json",类型="auto",来源="filesystem"):
         self.配置 = self._设置数据源(路径,类型,来源)._数据源转dict()
@@ -418,20 +442,68 @@ class 配置类:
     def 取值(self, key = "", 分隔符 = "."):
         return getDictValue(self.配置, key, 分隔符)
 
-    def 关联(self, 变量集 = {}, 关联表 = {}):
+    def 关联(self, 变量集, 关联表 = {}, 分隔符="."):
+        if not 关联表:
+            关联表 = {}
+            keyLocation = self._get_dict_keyLocation_list(变量集,分隔符=分隔符)
+            for i in keyLocation:
+                关联表[i] = i
         self.变量集 = 变量集
         self.关联表 = 关联表
         return self
 
-    def 重载(self, 分隔符 = "."):
+    def 重载(self, 分隔符 = ".", is_override_vars = True):
         # 加载配置
         self.配置 = self._数据源转dict()
 
-        # 覆写变量
-        for key in self.关联表.keys():
-            self.变量集[key] = getDictValue(self.配置, self.关联表.get(key), self.变量集.get(key) , 分隔符)
+        if is_override_vars:
+            # 覆写变量
+            for key in self.关联表.keys():
+                old_value = getDictValue(self.变量集,key,分隔符=分隔符)
+                now_value = getDictValue(self.配置, self.关联表.get(key), old_value , 分隔符)
+                setDictValue(self.变量集,key,now_value)
 
         return self
+
+
+    def set(self,路径="./配置文件.json",类型="auto",来源="filesystem"):
+        return self.加载(路径,类型,来源)
+
+    def get(self, key = "", 分隔符 = "."):
+        return self.取值(key, 分隔符)
+
+    def link(self, 变量集, 关联表 = {}, 分隔符="."):
+        return self.关联(变量集, 关联表, 分隔符=分隔符)
+
+    def reload(self, 分隔符 = ".", is_override_vars = True):
+        return self.重载(分隔符, is_override_vars)
+
+    def export(self, is_del_before = False, vars ={}):
+        config_filePath = self.数据源["路径"]
+        is_file_exist = os.path.exists(config_filePath)
+
+        if is_file_exist:
+            if is_del_before:
+                os.remove(config_filePath)
+            else:
+                return self
+
+        所在目录 = os.path.dirname(config_filePath)
+        if 所在目录:
+            if not os.path.exists(所在目录):
+                os.makedirs(所在目录)
+                
+        if not vars:
+            vars = self.变量集
+        with open(config_filePath, 'w', encoding='utf-8') as f:
+            json.dump(vars, f, ensure_ascii=False, indent=2)
+
+        return self
+
+
+    def all(self, 配置文件路径, 变量集, 类型="auto", 来源="filesystem", 关联表 = {}, 分隔符 = ".", is_del_before = False, export_vars = {}, is_override_vars = True):
+        return self.set(配置文件路径,类型,来源).link(变量集,关联表,分隔符=分隔符).export(is_del_before,export_vars).reload(分隔符, is_override_vars)
+
 
 
 配置 = 配置类.实例化()
