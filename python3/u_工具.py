131b3b44eb4e97d6ab4a7176bbda05cc671cf306
hlmio <52530012+hl-mio@users.noreply.github.com>
2020-12-24 11:55:37 +0800

    Update u_工具.py
    增加 to分类

--- python3/u_工具.py
+++ python3/u_工具.py
@@ -36,26 +36,6 @@ def 每x行取任意行_生成器(x, *args):
 def x分钟前的unix(minutes=30):
     return time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=minutes)).timetuple())
 
-def to_md5(data):
-    type_str = repr(type(data))
-    if type_str != "<class 'bytes'>" and type_str != "<class 'str'>":
-        data = json.dumps(data)
-    if repr(type(data)) == "<class 'str'>":
-        data = data.encode('utf-8')
-    md5 = hashlib.md5()
-    md5.update(data)
-    return md5.hexdigest()
-
-
-__to_变量名__pattren = re.compile(r'[\W+\w+]*?to_变量名\((\w+)\)')
-__to_变量名__变量名集 = []
-def to_变量名(变量):
-    global __to_变量名__变量名集
-    if not __to_变量名__变量名集:
-        __to_变量名__变量名集 = __to_变量名__pattren.findall(traceback.extract_stack(limit=2)[0][3])
-    return __to_变量名__变量名集.pop(0)
-
-
 def change_locals(frame, 修改表={}):
     frame.f_locals.update(修改表)
     ctypes.pythonapi.PyFrame_LocalsToFast(
@@ -197,6 +177,61 @@ def getAllFilePaths(baseFilePath, is_deep=True, rst_filePaths=[]):
 
 
 # endregion 未分类
+
+
+# region to
+
+def to_md5(data):
+    type_str = repr(type(data))
+    if type_str != "<class 'bytes'>" and type_str != "<class 'str'>":
+        data = json.dumps(data)
+    if repr(type(data)) == "<class 'str'>":
+        data = data.encode('utf-8')
+    md5 = hashlib.md5()
+    md5.update(data)
+    return md5.hexdigest()
+
+def to_uuid(去除中横线=True,使用随机数=True):
+    if 使用随机数:
+        id = uuid.uuid4()
+    else:
+        id = uuid.uuid1()
+    id = str(id)
+    if 去除中横线:
+        id = id.replace("-","")
+    return id
+
+__to_变量名__pattren = re.compile(r'[\W+\w+]*?to_变量名\((\w+)\)')
+__to_变量名__变量名集 = []
+def to_变量名(变量):
+    global __to_变量名__变量名集
+    if not __to_变量名__变量名集:
+        __to_变量名__变量名集 = __to_变量名__pattren.findall(traceback.extract_stack(limit=2)[0][3])
+    return __to_变量名__变量名集.pop(0)
+
+def to_str(obj):
+    return json.dumps(obj)
+
+def to_file(obj, 文件对象, ensure_ascii=False, indent=2):
+    return json.dump(obj, 文件对象, ensure_ascii=ensure_ascii, indent=indent)
+
+def to_dict(字符串or文件对象):
+    def from_str_to_dict():
+        return json.loads(字符串or文件对象)
+
+    def from_file_to_dict():
+        return json.load(字符串or文件对象)
+
+    def default():
+        raise Exception("参数类型未支持")
+
+    switch = {
+        "<class 'str'>": from_str_to_dict,
+        "<class '_io.TextIOWrapper'>": from_file_to_dict,
+    }
+    return switch.get(repr(type(字符串or文件对象)), default)()
+
+# endregion
 
 
 # region fileSystem
