a44e43b64d8a28aec827b6de385f0317d3084da3
hlmio <52530012+hl-mio@users.noreply.github.com>
2020-12-27 00:46:55 +0800

    更新 u_工具.py
    更新to类，新增时间转换方法

--- python3/u_工具.py
+++ python3/u_工具.py
@@ -4,8 +4,8 @@ import json
 import os
 import shutil
 import time
-import random
 import datetime
+import random
 import threading
 import re
 import traceback
@@ -33,9 +33,6 @@ def 每x行取任意行_生成器(x, *args):
             yield True
         else:
             yield False
-
-def x分钟前的unix(minutes=30):
-    return time.mktime((datetime.datetime.now() - datetime.timedelta(minutes=minutes)).timetuple())
 
 def change_locals(frame, 修改表={}):
     frame.f_locals.update(修改表)
@@ -180,7 +177,122 @@ def getAllFilePaths(baseFilePath, is_deep=True, rst_filePaths=[]):
 # endregion 未分类
 
 
-# region to
+# region to_x
+
+# region time  -- datetime.datetime是原点，是核心中间类
+
+时间字符串_模板 = "%Y-%m-%d %H:%M:%S"
+def to_datetime(字符串or时间戳or时间元组 = 0, 格式字符串=时间字符串_模板, 增加几秒=0, 增加几分钟=0, 增加几小时=0, 增加几天=0):
+    obj = 字符串or时间戳or时间元组
+
+    def from_str_to_datetime():
+        字符串 = obj # type:str
+        字符串 = 字符串.strip()
+        if 字符串=="" or 字符串=="0":
+            return get_now_datetime()
+        return datetime.datetime.strptime(字符串, 格式字符串)
+
+    def from_时间元组_to_datetime():
+        return datetime.datetime.fromtimestamp(obj)
+
+    def from_普通元组_to_datetime():
+        nonlocal obj
+        普通元组 = obj # type:tuple
+        if 普通元组.count() < 9:
+            补充个数 = 9 - 普通元组.count()
+            for i in range(补充个数):
+                普通元组 += (-1)
+        obj = time.mktime(普通元组)
+        return from_时间戳_to_datetime()
+
+    def from_时间戳_to_datetime():
+        时间戳 = obj  # type:float
+        if 时间戳==0:
+            return get_now_datetime()
+        return datetime.datetime.fromtimestamp(时间戳)
+
+    def from_datetime_to_datetime():
+        return obj
+
+    def default():
+        raise Exception(f"参数类型未支持：{repr(type(obj))}")
+
+    def get_now_datetime():
+        return datetime.datetime.now()
+
+    switch = {
+        "<class 'str'>": from_str_to_datetime,
+        "<class 'int'>": from_时间戳_to_datetime,
+        "<class 'float'>": from_时间戳_to_datetime,
+        "<class 'tuple'>": from_普通元组_to_datetime,
+        "<class 'time.struct_time'>": from_时间元组_to_datetime,
+        "<class 'datetime.datetime'>": from_datetime_to_datetime,
+    }
+    原点时间 = switch.get(repr(type(obj)), default)()
+
+    # 接下来处理时间的增减
+    增加的时间 = datetime.timedelta(seconds=增加几秒,minutes=增加几分钟,hours=增加几小时,days=增加几天)
+    return 原点时间 + 增加的时间
+
+def to_时间字符串(datetime_or_字符串or时间戳or时间元组 = 0, 格式字符串=时间字符串_模板, 增加几秒=0, 增加几分钟=0, 增加几小时=0, 增加几天=0):
+    时间对象 = to_datetime(datetime_or_字符串or时间戳or时间元组, 格式字符串, 增加几秒, 增加几分钟, 增加几小时, 增加几天)
+    return 时间对象.strftime(格式字符串)
+
+def to_时间戳(datetime_or_字符串or时间戳or时间元组 = 0, 增加几秒=0, 增加几分钟=0, 增加几小时=0, 增加几天=0):
+    时间对象 = to_datetime(datetime_or_字符串or时间戳or时间元组, 时间字符串_模板, 增加几秒, 增加几分钟, 增加几小时, 增加几天)
+    return time.mktime(时间对象.timetuple())
+
+def to_时间元组(datetime_or_字符串or时间戳or时间元组 = 0, 增加几秒=0, 增加几分钟=0, 增加几小时=0, 增加几天=0):
+    时间对象 = to_datetime(datetime_or_字符串or时间戳or时间元组, 时间字符串_模板, 增加几秒, 增加几分钟, 增加几小时, 增加几天)
+    return 时间对象.timetuple()
+
+def to_unix(datetime_or_字符串or时间戳or时间元组 = 0, 增加几秒=0, 增加几分钟=0, 增加几小时=0, 增加几天=0):
+    return to_时间戳(datetime_or_字符串or时间戳or时间元组, 增加几秒, 增加几分钟, 增加几小时, 增加几天)
+
+
+def to_now_datetime():
+    return to_datetime(0)
+
+def to_now_时间字符串():
+    return to_时间字符串(0)
+
+def to_now_时间戳():
+    return to_时间戳(0)
+
+def to_now_时间元组():
+    return to_时间元组(0)
+
+
+def x分钟前的unix(分钟数 = 0):
+    return to_unix(0, 增加几分钟= -分钟数)
+
+# endregion
+
+# region json
+
+def to_json_str(obj):
+    return json.dumps(obj)
+
+def to_json_file(obj, 文件对象, ensure_ascii=False, indent=2):
+    return json.dump(obj, 文件对象, ensure_ascii=ensure_ascii, indent=indent)
+
+def to_json_obj(字符串or文件对象):
+    def from_str_to_dict():
+        return json.loads(字符串or文件对象)
+
+    def from_file_to_dict():
+        return json.load(字符串or文件对象)
+
+    def default():
+        raise Exception("参数类型未支持")
+
+    switch = {
+        "<class 'str'>": from_str_to_dict,
+        "<class '_io.TextIOWrapper'>": from_file_to_dict,
+    }
+    return switch.get(repr(type(字符串or文件对象)), default)()
+
+# endregion
 
 def to_md5(data):
     type_str = repr(type(data))
@@ -210,27 +322,6 @@ def to_变量名(变量):
         __to_变量名__变量名集 = __to_变量名__pattren.findall(traceback.extract_stack(limit=2)[0][3])
     return __to_变量名__变量名集.pop(0)
 
-def to_str(obj):
-    return json.dumps(obj)
-
-def to_file(obj, 文件对象, ensure_ascii=False, indent=2):
-    return json.dump(obj, 文件对象, ensure_ascii=ensure_ascii, indent=indent)
-
-def to_dict(字符串or文件对象):
-    def from_str_to_dict():
-        return json.loads(字符串or文件对象)
-
-    def from_file_to_dict():
-        return json.load(字符串or文件对象)
-
-    def default():
-        raise Exception("参数类型未支持")
-
-    switch = {
-        "<class 'str'>": from_str_to_dict,
-        "<class '_io.TextIOWrapper'>": from_file_to_dict,
-    }
-    return switch.get(repr(type(字符串or文件对象)), default)()
 
 # endregion
 
