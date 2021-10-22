db36b597915d2239e472c72c31b97c486e554ae5
hlmio <52530012+hl-mio@users.noreply.github.com>
2021-10-22 21:31:31 +0800

    Update u_工具.py

--- python3/u_工具.py
+++ python3/u_工具.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021-10-14
+# @Time    : 2021-10-22
 # @Author  : hlmio
 import hashlib
 import json
@@ -21,6 +21,37 @@ from functools import wraps
 
 
 # region 未分类
+
+
+def get_file_rows(文件全路径, txt_分隔符=",", excel_sheet下标或名称=0, encoding="utf8", txt_is去掉所有空行=True):
+    if "xls" in 文件全路径 or "xlsx" in 文件全路径:
+        return _get_file_rows__excel(文件全路径, excel_sheet下标或名称, encoding)
+    else:
+        return _get_file_rows__txt(文件全路径, txt_分隔符, encoding, txt_is去掉所有空行)
+
+def _get_file_rows__txt(文件全路径, 分隔符=",", encoding="utf8", is去掉所有空行=True):
+    rows = []
+    with open(文件全路径,"r",encoding=encoding) as f:
+        for line in f.readlines():
+            line = line.rstrip("\n")
+            if is去掉所有空行 and not line:
+                continue
+            row = line.split(分隔符)
+            rows.append(row)
+    # 处理bom
+    bomList = ["\ufeff","\ufffe"]
+    for bom in bomList:
+        if rows and rows[0] and rows[0][0].startswith(bom):
+            rows[0][0] = rows[0][0].split(bom, 1)[1]
+    return rows
+
+def _get_file_rows__excel(文件全路径, sheet下标或名称=0, encoding="utf8"):
+    rows = []
+    sheet = get_excel_sheet(文件全路径, sheet下标或名称)
+    for i in range(get_excel_行数(sheet)):
+        row = get_excel_行(sheet, i)
+        rows.append(row)
+    return rows
 
 
 # region flask_api
@@ -81,34 +112,34 @@ excel类型 = {
 def get_excel_workbook(文件路径, 底层实现="xlrd"):
     底层实现 = 底层实现.lower()
     if 底层实现.lower() == "xlrd":
-        return get_excel_workbook__xlrd(文件路径)
+        return _get_excel_workbook__xlrd(文件路径)
     if 底层实现.lower() == "openpyxl":
-        return get_excel_workbook__openpyxl(文件路径)
+        return _get_excel_workbook__openpyxl(文件路径)
     return Exception("底层实现未支持")
-def get_excel_workbook__xlrd(文件路径):
+def _get_excel_workbook__xlrd(文件路径):
     return xlrd.open_workbook(文件路径)
-def get_excel_workbook__openpyxl(文件路径):
+def _get_excel_workbook__openpyxl(文件路径):
     return openpyxl.load_workbook(文件路径)
 
-def get_excel_sheet(文件路径, sheet下标=0, sheet名=None, 底层实现="xlrd"):
+def get_excel_sheet(文件路径, sheet下标或名称=0, 底层实现="xlrd"):
     底层实现 = 底层实现.lower()
     if 底层实现 == "xlrd":
-        return get_excel_sheet__xlrd(文件路径,sheet下标,sheet名)
+        return _get_excel_sheet__xlrd(文件路径, sheet下标或名称)
     if 底层实现 == "openpyxl":
-        return get_excel_sheet__openpyxl(文件路径,sheet下标,sheet名)
+        return _get_excel_sheet__openpyxl(文件路径, sheet下标或名称)
     return Exception("底层实现未支持")
-def get_excel_sheet__xlrd(文件路径, sheet下标=0, sheet名=None):
-    workbook = get_excel_workbook__xlrd(文件路径)
-    if sheet名:
-        return workbook.sheet_by_name(sheet名)
+def _get_excel_sheet__xlrd(文件路径, sheet下标或名称=0):
+    workbook = _get_excel_workbook__xlrd(文件路径)
+    if isinstance(sheet下标或名称, str):
+        return workbook.sheet_by_name(sheet下标或名称)
     else:
-        return workbook.sheet_by_index(int(sheet下标))
-def get_excel_sheet__openpyxl(文件路径, sheet下标=0, sheet名=None):
-    workbook = get_excel_workbook__openpyxl(文件路径)
-    if sheet名:
-        return workbook.get_sheet_by_name(sheet名)
+        return workbook.sheet_by_index(sheet下标或名称)
+def _get_excel_sheet__openpyxl(文件路径, sheet下标或名称):
+    workbook = _get_excel_workbook__openpyxl(文件路径)
+    if isinstance(sheet下标或名称, str):
+        return workbook.get_sheet_by_name(sheet下标或名称)
     else:
-        return workbook.worksheets[sheet下标]
+        return workbook.worksheets[sheet下标或名称]
 
 
 
@@ -503,7 +534,6 @@ def 启动定时任务_阻塞主线程():
 # endregion
 
 
-
 # region shell
 try:
     import subprocess
@@ -522,6 +552,151 @@ def shell(cmd, stdout=subprocess.PIPE, encoding="utf8", shell=True, check=True, 
     return subprocess.run(cmd, stdout=stdout, encoding=encoding, shell=shell, check=check, **kwargs)\
                     .stdout
 # endregion shell
+
+
+# region 配置相关
+try:
+    import configparser
+except: pass
+
+
+
+def _configparser_to_dict(config):
+    my_dict = dict(config._sections)
+    for key in my_dict:
+        my_dict[key] = dict(my_dict[key])
+    return my_dict
+
+
+class 配置类:
+
+    @staticmethod
+    def 实例化():
+        return 配置类()
+
+    def __init__(self):
+        self.数据源 = {}
+        self.配置 = {}
+        self.变量集 = {}
+        self.关联表 = {}
+
+    def _数据源转dict(self):
+        路径 = self.数据源["路径"]
+        类型 = self.数据源["类型"]
+        来源 = self.数据源["来源"]
+
+        if (not 类型) or 类型 == "auto":
+            类型 = os.path.splitext(路径)[-1][1:].lower()
+
+        try:
+            if 类型 == "json":
+                if 来源 == "filesystem" or 来源 == "file_system" or 来源 == "file" or 来源 == "fs":
+                    dict配置 = json.loads(pathlib.Path(路径).read_text(encoding='UTF-8'))
+                else:
+                    raise Exception("配置加载失败，来源不支持")
+            else:
+                config = configparser.ConfigParser()
+                config.read(路径, encoding='UTF-8')
+                dict配置 = _configparser_to_dict(config)
+        except:
+            dict配置 = {}
+
+        return dict配置
+
+    def _设置数据源(self, 路径, 类型, 来源):
+        self.数据源["路径"] = 路径
+        self.数据源["类型"] = 类型.lower()
+        self.数据源["来源"] = 来源.lower()
+        return self
+
+    def _get_dict_keyLocation_list(self, mydict={}, 分隔符=".", 前缀="", rstList=[]):
+        for i in mydict.keys():
+            if not 前缀:
+                临时前缀 = f"{i}"
+            else:
+                临时前缀 = f"{前缀}{分隔符}{i}"
+
+            if isinstance(mydict[i], dict):
+                self._get_dict_keyLocation_list(mydict[i], 分隔符, 临时前缀, rstList)
+            else:
+                rstList.append(临时前缀)
+
+        return rstList
+
+    def 加载(self, 路径="./配置文件.json", 类型="auto", 来源="filesystem"):
+        self.配置 = self._设置数据源(路径, 类型, 来源)._数据源转dict()
+        return self
+
+    def 取值(self, key="", 分隔符="."):
+        return getDictValue(self.配置, key, 分隔符)
+
+    def 关联(self, 变量集, 关联表={}, 分隔符="."):
+        if not 关联表:
+            关联表 = {}
+            keyLocation = self._get_dict_keyLocation_list(变量集, 分隔符=分隔符)
+            for i in keyLocation:
+                关联表[i] = i
+        self.变量集 = 变量集
+        self.关联表 = 关联表
+        return self
+
+    def 重载(self, 分隔符=".", is_override_vars=True):
+        # 加载配置
+        self.配置 = self._数据源转dict()
+
+        if is_override_vars:
+            # 覆写变量
+            for key in self.关联表.keys():
+                old_value = getDictValue(self.变量集, key, 分隔符=分隔符)
+                now_value = getDictValue(self.配置, self.关联表.get(key), old_value, 分隔符)
+                setDictValue(self.变量集, key, now_value)
+
+        return self
+
+    def set(self, 路径="./配置文件.json", 类型="auto", 来源="filesystem"):
+        return self.加载(路径, 类型, 来源)
+
+    def get(self, key="", 分隔符="."):
+        return self.取值(key, 分隔符)
+
+    def link(self, 变量集, 关联表={}, 分隔符="."):
+        return self.关联(变量集, 关联表, 分隔符=分隔符)
+
+    def reload(self, 分隔符=".", is_override_vars=True):
+        return self.重载(分隔符, is_override_vars)
+
+    def export(self, is_del_before=False, vars={}):
+        config_filePath = self.数据源["路径"]
+        is_file_exist = os.path.exists(config_filePath)
+
+        if is_file_exist:
+            if is_del_before:
+                os.remove(config_filePath)
+            else:
+                return self
+
+        所在目录 = os.path.dirname(config_filePath)
+        if 所在目录:
+            if not os.path.exists(所在目录):
+                os.makedirs(所在目录)
+
+        if not vars:
+            vars = self.变量集
+        with open(config_filePath, 'w', encoding='utf-8') as f:
+            json.dump(vars, f, ensure_ascii=False, indent=2)
+
+        return self
+
+    def all(self, 配置文件路径, 变量集, 类型="auto", 来源="filesystem", 关联表={}, 分隔符=".", is_del_before=False, export_vars={},
+            is_override_vars=True):
+        return self.set(配置文件路径, 类型, 来源).link(变量集, 关联表, 分隔符=分隔符).export(is_del_before, export_vars).reload(分隔符,
+                                                                                                          is_override_vars)
+
+
+配置 = 配置类.实例化()
+
+
+# endregion 配置相关
 
 
 # region dao
@@ -839,151 +1014,6 @@ def mysql(new_conf={}):
 
 
 # endregion dao
-
-
-# region 配置相关
-try:
-    import configparser
-except: pass
-
-
-
-def _configparser_to_dict(config):
-    my_dict = dict(config._sections)
-    for key in my_dict:
-        my_dict[key] = dict(my_dict[key])
-    return my_dict
-
-
-class 配置类:
-
-    @staticmethod
-    def 实例化():
-        return 配置类()
-
-    def __init__(self):
-        self.数据源 = {}
-        self.配置 = {}
-        self.变量集 = {}
-        self.关联表 = {}
-
-    def _数据源转dict(self):
-        路径 = self.数据源["路径"]
-        类型 = self.数据源["类型"]
-        来源 = self.数据源["来源"]
-
-        if (not 类型) or 类型 == "auto":
-            类型 = os.path.splitext(路径)[-1][1:].lower()
-
-        try:
-            if 类型 == "json":
-                if 来源 == "filesystem" or 来源 == "file_system" or 来源 == "file" or 来源 == "fs":
-                    dict配置 = json.loads(pathlib.Path(路径).read_text(encoding='UTF-8'))
-                else:
-                    raise Exception("配置加载失败，来源不支持")
-            else:
-                config = configparser.ConfigParser()
-                config.read(路径, encoding='UTF-8')
-                dict配置 = _configparser_to_dict(config)
-        except:
-            dict配置 = {}
-
-        return dict配置
-
-    def _设置数据源(self, 路径, 类型, 来源):
-        self.数据源["路径"] = 路径
-        self.数据源["类型"] = 类型.lower()
-        self.数据源["来源"] = 来源.lower()
-        return self
-
-    def _get_dict_keyLocation_list(self, mydict={}, 分隔符=".", 前缀="", rstList=[]):
-        for i in mydict.keys():
-            if not 前缀:
-                临时前缀 = f"{i}"
-            else:
-                临时前缀 = f"{前缀}{分隔符}{i}"
-
-            if isinstance(mydict[i], dict):
-                self._get_dict_keyLocation_list(mydict[i], 分隔符, 临时前缀, rstList)
-            else:
-                rstList.append(临时前缀)
-
-        return rstList
-
-    def 加载(self, 路径="./配置文件.json", 类型="auto", 来源="filesystem"):
-        self.配置 = self._设置数据源(路径, 类型, 来源)._数据源转dict()
-        return self
-
-    def 取值(self, key="", 分隔符="."):
-        return getDictValue(self.配置, key, 分隔符)
-
-    def 关联(self, 变量集, 关联表={}, 分隔符="."):
-        if not 关联表:
-            关联表 = {}
-            keyLocation = self._get_dict_keyLocation_list(变量集, 分隔符=分隔符)
-            for i in keyLocation:
-                关联表[i] = i
-        self.变量集 = 变量集
-        self.关联表 = 关联表
-        return self
-
-    def 重载(self, 分隔符=".", is_override_vars=True):
-        # 加载配置
-        self.配置 = self._数据源转dict()
-
-        if is_override_vars:
-            # 覆写变量
-            for key in self.关联表.keys():
-                old_value = getDictValue(self.变量集, key, 分隔符=分隔符)
-                now_value = getDictValue(self.配置, self.关联表.get(key), old_value, 分隔符)
-                setDictValue(self.变量集, key, now_value)
-
-        return self
-
-    def set(self, 路径="./配置文件.json", 类型="auto", 来源="filesystem"):
-        return self.加载(路径, 类型, 来源)
-
-    def get(self, key="", 分隔符="."):
-        return self.取值(key, 分隔符)
-
-    def link(self, 变量集, 关联表={}, 分隔符="."):
-        return self.关联(变量集, 关联表, 分隔符=分隔符)
-
-    def reload(self, 分隔符=".", is_override_vars=True):
-        return self.重载(分隔符, is_override_vars)
-
-    def export(self, is_del_before=False, vars={}):
-        config_filePath = self.数据源["路径"]
-        is_file_exist = os.path.exists(config_filePath)
-
-        if is_file_exist:
-            if is_del_before:
-                os.remove(config_filePath)
-            else:
-                return self
-
-        所在目录 = os.path.dirname(config_filePath)
-        if 所在目录:
-            if not os.path.exists(所在目录):
-                os.makedirs(所在目录)
-
-        if not vars:
-            vars = self.变量集
-        with open(config_filePath, 'w', encoding='utf-8') as f:
-            json.dump(vars, f, ensure_ascii=False, indent=2)
-
-        return self
-
-    def all(self, 配置文件路径, 变量集, 类型="auto", 来源="filesystem", 关联表={}, 分隔符=".", is_del_before=False, export_vars={},
-            is_override_vars=True):
-        return self.set(配置文件路径, 类型, 来源).link(变量集, 关联表, 分隔符=分隔符).export(is_del_before, export_vars).reload(分隔符,
-                                                                                                          is_override_vars)
-
-
-配置 = 配置类.实例化()
-
-
-# endregion 配置相关
 
 
 # region to_xxx
@@ -1771,15 +1801,15 @@ class ListStream:
 
 class DictStream(ListStream):
     def __init__(self, my_dict={}):
-        self.list = self.dict_to_list(my_dict)
+        self.list = self._dict_to_list(my_dict)
 
     def collect(self, is_to_dict=True):
         if is_to_dict:
-            return self.list_to_dict(self.list)
+            return self._list_to_dict(self.list)
         else:
             return self.list
 
-    def dict_to_list(self, old_dict):
+    def _dict_to_list(self, old_dict):
         new_list = []
         for i in old_dict.keys():
             temp_dict = {}
@@ -1788,7 +1818,7 @@ class DictStream(ListStream):
             new_list.append(temp_dict)
         return new_list
 
-    def list_to_dict(self, old_list):
+    def _list_to_dict(self, old_list):
         new_dict = {}
         for i in old_list:
             new_dict[i["key"]] = i["value"]
